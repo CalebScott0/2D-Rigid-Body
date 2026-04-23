ee6611503257c6c741caf2a4cf5d1980217f2a6f
Caleb Scott <calebbenjaminscott@pm.me>
2026-04-22 21:50:04 -0500

    point renders on screen

--- README.md
+++ README.md
@@ -5,7 +5,17 @@
  cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 ```
 
-## Build with debug symbols and run
+## Build with debug symbols and run with debugger
 ```
  cmake --build build --target debug
+```
+
+## Run normally with either
+```
+ cmake --build build
+ ./build/debug/main
+```
+or
+```
+ cmake --build build --target run
 ```
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <vector>
+
+void drawCircle(SDL_Renderer*);
 
 int main(int argc, char* argv[])
 {
@@ -32,8 +35,9 @@ int main(int argc, char* argv[])
         // rendering draws over whatever was drawn before
         SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE); // SDL_ALPHA_OPAQUE just a macro for 255 (max alpha)
         SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE);
-        SDL_RenderPoint(renderer, 300, 150);
+
+        drawCircle(renderer);
+
         SDL_RenderPresent(renderer);
     }
 
@@ -44,4 +48,25 @@ int main(int argc, char* argv[])
     SDL_Quit();
 
     return 0;
+}
+
+void drawCircle(SDL_Renderer* renderer) // TODO: Expand to have x and y coordinates
+{
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE);
+
+    // SDL_FPoint
+    // {
+    //     float x;
+    //     float y;
+    // }
+
+    std::vector<SDL_FPoint> points;
+    points.reserve(100);
+    // SDL_RenderPoints...
+    
+    // we will start by giving our circle a fixed radius (20 pixels)
+    // we must have one centrally tracked point that all others relate to
+    //  check distance from center?
+
+
 }
